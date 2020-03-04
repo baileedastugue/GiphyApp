5e96996854de17679839907fdbdb42f0d023399d
Bailee Dastugue <bailee.dast@gmail.com>
2020-03-04 15:46:52 -0600

    added music array, buttons pushed to html

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,1 +1,13 @@
-// $('.dropdown-toggle').dropdown();
+var musicTopics = ["Annie Clark", "Billy Joel", "The Strokes", "The Crystals"]
+
+function addButton () {
+    for (var i = 0; i < musicTopics.length; i++) {
+        var newButton = $("<button>");
+        newButton.attr("class", "btn btn-primary music-btn");
+        newButton.attr("data-name", musicTopics[i]);
+        newButton.text(musicTopics[i]);
+        $("#musicButtons-container").append(newButton );
+    }
+}
+
+addButton();
