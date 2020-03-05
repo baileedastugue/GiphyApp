3b3d09ccae9a01ecbb0abbdd9e61d5c770b49443
Bailee Dastugue <bailee.dast@gmail.com>
2020-03-04 20:46:41 -0600

    randome gifs add with each button click, add button works again

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,4 +1,6 @@
-var musicTopics = ["Annie Clark", "Billy Joel", "The Strokes", "The Crystals"]
+$( document ).ready(function() {
+
+var musicTopics = ["Annie Clark", "Billy Joel", "The Strokes", "Talking Heads"]
 var $this;
 
 function addButton () {
@@ -7,7 +9,7 @@ function addButton () {
         newButton.attr("class", "btn btn-primary music-btn");
         newButton.attr("data-name", musicTopics[i]);
         newButton.text(musicTopics[i]);
-        $("#musicButtons-container").append(newButton );
+        $("#musicButtons-container").append(newButton);
     }
 }
 
@@ -15,18 +17,64 @@ addButton();
 
 $("#add-music").on("click", function(){
     var addition = $("#music-input").val().trim();
-    musicTopics.push(addition);
+    musicTopics.push(addition); 
     $("#musicButtons-container").empty();
     addButton();
-})
+    return false;
+});
+
+function shuffle(array) {
+    var currentIndex = array.length, temporaryValue, randomIndex;
+  
+    // While there remain elements to shuffle...
+    while (0 !== currentIndex) {
+  
+      // Pick a remaining element...
+      randomIndex = Math.floor(Math.random() * currentIndex);
+      currentIndex -= 1;
+  
+      // And swap it with the current element.
+      temporaryValue = array[currentIndex];
+      array[currentIndex] = array[randomIndex];
+      array[randomIndex] = temporaryValue;
+    }
+  
+    return array;
+  }
 
 function displayGIFS () {
-    
     var musicInfo = $this.attr("data-name");
-    console.log(musicInfo);
+    var queryURL = "https://api.giphy.com/v1/gifs/search?q=" +
+        musicInfo + "&api_key=jbVjecxyl97zlwipqSEjHVb4NqK2PG5d";
+
+    $.ajax({
+        url: queryURL,
+        method: "GET"
+        })
+        .then(function(response) {
+            var results = response.data;
+            var randomArray = [];
+
+            for (var i = 0; i < 25; i++) {
+                randomArray.push(i);
+            }
+            randomArray = shuffle(randomArray);
+            randomArray = randomArray.splice(0, 10);
+
+            for (var i = 0; i < randomArray.length; i++){
+                var randomNum = randomArray[i];
+                var imgTag = $("<img>");
+                imgTag.attr("src", results[randomNum].images.fixed_width.url);
+                $("#gif-container").append(imgTag);
+            }
+        })
+
 }
 
 $(document).on("click", ".music-btn", function() {
     $this = $(this);
+    $("#gif-container").empty();
     displayGIFS()
 })
+
+});
